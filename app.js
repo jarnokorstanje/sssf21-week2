ba23b8b05aa84e1939e2263149820946f9645caf
jarnokorstanje <kors0023@hz.nl>
2021-03-17 17:34:49 +0200

    add route parameters

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 'use strict';
 const express = require('express');
+const catRoute = require('./routes/catRoute');
 const app = express();
 const port = 3000;
 
-app.get('/cat', (req, res) => {
-  res.send('From this endpoint you can get cats.')
-});
+app.use(express.static('week2_public_html'));
+
+app.use('/cat', catRoute);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,2 +1,25 @@
 'use strict';
-// catRoute
+
+const express = require('express');
+const {cat_list_get} = require('../controllers/catController');
+const router = express.Router();
+
+router.get('/', cat_list_get);
+
+router.get('/:id', (req, res) => {
+  res.send('cat with id ' + req.params.id);
+});
+
+router.post('/', (req, res) => {
+  res.send('post');
+});
+
+router.put('/', (req, res) => {
+  res.send('put');
+});
+
+router.delete('/', (req, res) => {
+  res.send('delete');
+});
+
+module.exports = router;
